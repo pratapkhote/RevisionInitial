11136fb5e87d2020f30a21badad0bc8475e63b2b
pratapkhote <prathap@prakat.in>
2023-07-21 22:42:39 +0530

    [pratapkhote] Added new changes.

--- src/test/java/Test/OpenGoogle.java
+++ src/test/java/Test/OpenGoogle.java
@@ -9,20 +9,14 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OpenGoogle {
-  @Test
-  public void googleHomepage() {
+	@Test
+	public void googleHomepage() {
 
-	  WebDriverManager.chromedriver().setup();
-	 
-		/*
-		 * System.setProperty("webdriver.chrome.driver",
-		 * "E:\\AutomationPathFiles\\chromedriver.exe"); ChromeOptions co = new
-		 * ChromeOptions(); co.addArguments("--remote-allow-origins=*"); WebDriver
-		 * driver = new ChromeDriver(co);
-		 */
-	 WebDriver driver=new ChromeDriver();
-		 driver.get("https://www.google.com");
-		
+		System.setProperty("webdriver.chrome.driver", "E:\\AutomationPathFiles\\chromedriver.exe");
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(co);
+		driver.get("https://www.google.com");
 
-  }
+	}
 }
